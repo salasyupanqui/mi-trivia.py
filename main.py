77593a17ff50c0995cdde7aca8524ef3d6a332ea
salasyupanqui <113658218+salasyupanqui@users.noreply.github.com>
2022-09-16 13:55:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ print("a) Linus Torvalds")
 print("b) Bill Gates")
 print("c) Mark Zuckerberg")
 print("d) Dennis Ritchie")
-
 # Almacenamos la respuesta del usuario en la variable "respuesta_1"
 respuesta_1 = input("\nTu respuesta: ")
 if respuesta_1 == "b":
@@ -31,10 +30,53 @@ else:
 
 print("2) ¿quien fue el creador de python?")
 
-print("3) ¿quien fue el precursor de la creacion de gnu Linux?")
+print("a)Jhon backus")
+print("b)Ada lovelace")
+print("c)Short range")
+print("d)Guido van rossum")
 
+respuesta_2 = input("\nTu respuesta: ")
+if respuesta_2 == "d":
+    print("respuesta correcta")
+else:
+    print("fallaste")
+
+
+print("3) ¿quien fue el precursor de la creacion de gnu Linux?")
+print("a)Linus torvalds")
+print("b)Ghido van rossum")
+print("c)Kim dotcom")
+print("d)Jean salas")
+
+respuesta_3 = input("\nTu respuesta: ")
+if respuesta_3 == "a":
+    print("respuesta correcta")
+else:
+    print("fallaste")
 print(
     "4) ¿que lenguaje de programacion fue el precursor de la mayoria de los lenguajes de programacion?"
 )
+print("a)Fortran")
+print("b)Lisp")
+print("c)Cobol")
+print("d)Todas las anteriores")
 
+respuesta_4 = input("\nTu respuesta: ")
+if respuesta_4 == "d":
+    print("respuesta correcta")
+else:
+    print("fallaste")
 print("5) ¿con que lenguaje de programacion se hizo Netflix?")
+print("a)Java script")
+print("b)Java")
+print("c)Python")
+print("d)Pascal")
+
+respuesta_5 = input("\nTu respuesta: ")
+if respuesta_5 == "c":
+    print("respuesta correcta")
+else:
+    print("fallaste")
+    
+ #en la siguiente sesion agregaremos mas funcionalidades y trabajaremos las preguntas con funciones
+ #don´t repeat yourself
